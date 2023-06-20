97cb6828f0b1293316211cdc2070e16fb601b412
Jon Poole <jonfpoole@gmail.com>
2023-06-20 15:37:45 +0100

    Use splitlist

--- tools/please_go/generate/generate.go
+++ tools/please_go/generate/generate.go
@@ -406,8 +406,8 @@ func nameForLibInPkg(module, pkg string) string {
 // trimPath is like strings.TrimPrefix but is path aware. It removes base from target if target starts with base,
 // otherwise returns target unmodified.
 func trimPath(target, base string) string {
-	baseParts := strings.Split(filepath.Clean(base), "/")
-	targetParts := strings.Split(filepath.Clean(target), "/")
+	baseParts := filepath.SplitList(base)
+	targetParts := filepath.SplitList(target)
 
 	if len(targetParts) < len(baseParts) {
 		return target
