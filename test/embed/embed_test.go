48379aa03ddfd0aaa00dd270e81c97aabf495177
Peter Ebden <peter.ebden@gmail.com>
2023-10-01 10:12:27 +0100

    Add test

--- test/embed/embed_test.go
+++ test/embed/embed_test.go
@@ -10,3 +10,14 @@ import (
 func TestLibEmbed(t *testing.T) {
 	assert.Equal(t, "hello", strings.TrimSpace(hello))
 }
+
+func TestEmbedDir(t *testing.T) {
+	_, err := subdir.ReadFile("subdir/_test.txt")
+	assert.Error(t, err)
+}
+
+func TestEmbedDirAll(t *testing.T) {
+	b, err := subdir.ReadFile("subdir/_test.txt")
+	assert.NoError(t, err)
+	assert.Equal(t, "hello", strings.TrimSpace(string(b)))
+}
