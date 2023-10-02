cad8b260fa5b9f576ed9a7624c14db597be66eac
Peter Ebden <peter.ebden@gmail.com>
2023-10-02 14:04:42 +0100

    Support :all syntax correctly when embedding (#161)
    * Add test
    
    * Implement

--- test/embed/embed.go
+++ test/embed/embed.go
@@ -1,6 +1,12 @@
 package embed
 
-import _ "embed"
+import "embed"
 
 //go:embed hello.txt
 var hello string
+
+//go:embed subdir
+var subdir embed.FS
+
+//go:embed all:subdir
+var subdirAll embed.FS
