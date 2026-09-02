9b287a28ba0e25f76c9ed0d691b43f05721547e2
Chris Novakovic <chris@chrisn.me.uk>
2026-09-02 14:32:30 +0100

    Add `definitions` parameter to `go_repo` (#378)
    This sets the given value of `definitions` on `go_binary` and
    `cgo_binary` targets generated within the subrepo. This is useful for
    replicating the behaviour of third-party binary build pipelines in
    Please (e.g. by allowing version strings to be set dynamically during
    linking).

---
+++ test/go_repo_definitions/test_repo/third_party/go/module/cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	tstring "test.please.build/module/string"
+)
+
+func main() {
+	fmt.Println(tstring.GetString())
+}
---
+++ test/go_repo_definitions/test_repo/third_party/go/module/go.mod
@@ -0,0 +1,3 @@
+module test.please.build/module
+
+go 1.26.0
